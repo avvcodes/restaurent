ccb65e25d91eaff00b53a394d93f5008c60773af
Venkat <allakavenkatvinay@gmail.com>
2022-01-05 21:40:12 +0530

    added cart and orders pages

---
+++ pages/Cart.jsx
@@ -0,0 +1,84 @@
+import styles from "../styles/Cart.module.css";
+import Image from "next/image";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/img/pizza.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td className={styles.price}>$19.90</td>
+            <td className={styles.quantity}>1</td>
+            <td className={styles.total}>$19.90</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/img/pizza.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td className={styles.price}>$19.90</td>
+            <td className={styles.quantity}>1</td>
+            <td className={styles.total}>$19.90</td>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$19.90
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$19.90
+          </div>
+          <button className={styles.button}>CHECKOUT NOW</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ pages/orders/[id].js
@@ -0,0 +1,88 @@
+import styles from "../../styles/Order.module.css";
+import Image from "next/image";
+
+const Order = () => {
+  const status = 0;
+  const statusClass = (index) => {
+    if (index - status < 1) return styles.done;
+    if (index - status === 1) return styles.inProgress;
+    if (index - status > 1) return styles.undone;
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.row}>
+          <table className={styles.table}>
+            <tr className={styles.tr}>
+              <th>Order Id</th>
+              <th>Customer</th>
+              <th>Address</th>
+              <th>Total</th>
+            </tr>
+            <tr>
+              <td>
+                <span className={styles.id}>34324</span>
+              </td>
+              <td>
+                <span className={styles.name}>john</span>
+              </td>
+              <td className={styles.address}>NEWYORK,USA</td>
+              <td className={styles.total}>$19.90</td>
+            </tr>
+          </table>
+        </div>
+        <div className={styles.row}>
+          <div className={statusClass(0)}>
+            <Image alt="" src="/img/paid.png" width={30} height={30} />
+            <span>Payment</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/img/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+          <div className={statusClass(1)}>
+            <Image alt="" src="/img/bake.png" width={30} height={30} />
+            <span>Preparing</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/img/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+          <div className={statusClass(2)}>
+            <Image alt="" src="/img/bike.png" width={30} height={30} />
+            <span>On the way</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/img/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+          <div className={statusClass(3)}>
+            <Image alt="" src="/img/delivered.png" width={30} height={30} />
+            <span>Delivered</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/img/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$19.90
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$19.90
+          </div>
+          <button className={styles.button} disabled>
+            PAID
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+// 1:27:00
+
+export default Order;
